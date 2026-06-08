822d8b4e6c8d08f680024bc586d704ce1a07d124
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2026-06-08 20:07:06 +0300

    fix(login): fix login page crash cannot set property textcontent of null

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1039,25 +1039,32 @@ class LoginPage extends Page {
 
                 // Update password section with user info
                 const userEl = this.$('#selected-user');
-                userEl.querySelector('.login-user-name').textContent = user.Name;
+                const nameEl = userEl?.querySelector('.login-user-name, .user-name');
+                if (nameEl) {
+                    nameEl.textContent = user.Name;
+                }
 
-                const img = userEl.querySelector('.login-user-avatar');
-                const placeholder = userEl.querySelector('.user-avatar-placeholder');
+                const img = userEl?.querySelector('.login-user-avatar, .user-avatar');
+                const placeholder = userEl?.querySelector('.user-avatar-placeholder');
 
-                placeholder.textContent = user.Name.charAt(0).toUpperCase();
+                if (placeholder) {
+                    placeholder.textContent = user.Name.charAt(0).toUpperCase();
+                }
 
-                if (user.PrimaryImageTag) {
-                    const params = imageService.getParams('avatar');
-                    img.src = api.getUserImageUrl(user.Id, {
-                        maxWidth: params.maxWidth,
-                        quality: params.quality
-                    });
-                    img.classList.remove('hidden');
-                    placeholder.classList.add('hidden');
-                } else {
-                    img.src = '';
-                    img.classList.add('hidden');
-                    placeholder.classList.remove('hidden');
+                if (img) {
+                    if (user.PrimaryImageTag) {
+                        const params = imageService.getParams('avatar');
+                        img.src = api.getUserImageUrl(user.Id, {
+                            maxWidth: params.maxWidth,
+                            quality: params.quality
+                        });
+                        img.classList.remove('hidden');
+                        placeholder?.classList.add('hidden');
+                    } else {
+                        img.src = '';
+                        img.classList.add('hidden');
+                        placeholder?.classList.remove('hidden');
+                    }
                 }
 
                 // Clear password input
